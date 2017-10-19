7a6c5aa8ef6a61fc7f47addfeb95c7fbd4af49ca
Eric Abell <ericabell@users.noreply.github.com>
2017-10-19 15:16:37 -0500

    Remove JsonResponse in favor of the rest_framework Response

--- snippets/views.py
+++ snippets/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
