14bf0c821e6d07707adb4f91710c8f0799c7af8c
Eric Abell <ericabell@users.noreply.github.com>
2017-10-19 14:48:49 -0500

    Add get, put and delete for a single snippet

--- snippets/views.py
+++ snippets/views.py
@@ -24,3 +24,34 @@ def snippet_list(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def snippet_detail(request, pk):
+    """
+    Retrieve, update or delete a code snippet
+    """
+    # Try to locate the requested snippet
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return HttpResponse(status=404) # return 404 if not found
+
+    if request.method == 'GET':
+        # serialize the snippet and send JSON back
+        serializer = SnippetSerializer(snippet)
+        return JsonResponse(serializer.data)
+
+    if request.method == 'PUT':
+        # parse the JSON, create the Snippet Object, save it
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        # delete the snippet from db, return success code
+        snippet.delete()
+        return HttpResponse(status=204)
