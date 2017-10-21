ac01f59f60f9300d098f896028fdcb88c4e53667
Eric Abell <ericabell@users.noreply.github.com>
2017-10-20 21:09:40 -0500

    Redefine the url() to use the new named viewsets

--- snippets/urls.py
+++ snippets/urls.py
@@ -25,13 +25,13 @@ user_detail = UserViewSet.as_view({
     'get': 'retrieve'
 })
 
-urlpatterns = [
-    url(r'^$', views.api_root),
-    url(r'^snippets/$', views.SnippetList.as_view(), name='snippet-list'),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view(), name='snippet-detail'),
-    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view(), name='snippet-highlight'),
-    url(r'^users/$', views.UserList.as_view(), name='user-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail'),
-]
+urlpatterns = format_suffix_patterns([
+    url(r'^$', api_root),
+    url(r'^snippets/$', snippet_list, name='snippet-list'),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', snippet_detail, name='snippet-detail'),
+    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', snippet_highlight, name='snippet-highlight'),
+    url(r'^users/$', user_list, name='user-list'),
+    url(r'^users/(?P<pk>[0-9]+)/$', user_detail, name='user-detail')
+])
 
 urlpatterns = format_suffix_patterns(urlpatterns)
