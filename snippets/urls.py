944451f4b639feacfb201338aba5ba0ae5f28967
Eric Abell <ericabell@users.noreply.github.com>
2017-10-20 21:08:59 -0500

    Create a set of view from the new ViewSets for Snippet and User

--- snippets/urls.py
+++ snippets/urls.py
@@ -2,6 +2,29 @@ from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
+from snippets.views import SnippetViewSet, UserViewSet, api_root
+from rest_framework import renderers
+
+snippet_list = SnippetViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+snippet_detail = SnippetViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+snippet_highlight = SnippetViewSet.as_view({
+    'get': 'highlight'
+}, renderer_classes=[renderers.StaticHTMLRenderer])
+user_list = UserViewSet.as_view({
+    'get': 'list'
+})
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve'
+})
+
 urlpatterns = [
     url(r'^$', views.api_root),
     url(r'^snippets/$', views.SnippetList.as_view(), name='snippet-list'),
