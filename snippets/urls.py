12b03569ec23a40e90b60573a7c2c004ae26f897
Eric Abell <ericabell@users.noreply.github.com>
2017-10-22 20:17:44 -0500

    Add url map to the auto generated schemaview

--- snippets/urls.py
+++ snippets/urls.py
@@ -4,6 +4,8 @@ from rest_framework.routers import DefaultRouter
 
 from rest_framework.schemas import get_schema_view
 
+schema_view = get_schema_view(title='Pastebin API')
+
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet)
@@ -12,6 +14,7 @@ router.register(r'users', views.UserViewSet)
 # The API URLs are now determined automatically by the router.
 # Additionally, we include the login URLs for the browsable API.
 urlpatterns = [
+    url(r'^schema/$, schema_view),
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
